68100645496fe866f61709fde09c6cd95d0d6e15
Карина Дроздова <K.Drozd33@yandex.ru>
2020-05-22 13:55:52 +0300

    .

--- Task 9.2/Program.cs
+++ Task 9.2/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Task_9._2
 {
@@ -10,6 +11,28 @@ namespace Task_9._2
     {
         static void Main(string[] args)
         {
+            try
+            {
+                StreamReader sr = new StreamReader("text.txt");
+                StreamWriter sw = new StreamWriter("text2.txt");
+                while (sr.EndOfStream != true)
+                {
+                    string s = sr.ReadLine();
+                    string s2 = "";
+                    for (int i = 1; i < s.Length; i += 2)
+                    {
+                        s2 += s[i];
+                    }
+                    sw.WriteLine(s2);
+                }
+                sr.Close();
+                sw.Close();
+            }
+            catch(Exception E)
+            {
+                Console.WriteLine(E.Message);
+            }
+            Console.ReadKey();
         }
     }
 }
